f8149a10622f619de13c53f5339c6409af5a6244
Med khalil benkhelil <101598648+medkhalilbk@users.noreply.github.com>
2023-02-07 19:20:04 +0100

    version 2

---
+++ src/app/ads/ads.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ads',
+  templateUrl: './ads.component.html',
+  styleUrls: ['./ads.component.css']
+})
+export class AdsComponent {
+title: string;
+constructor(){
+  this.title ="NEKRETNINE U TREBINJU"
+}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SliderComponent } from './slider/slider.component';
+import { AdsComponent } from './ads/ads.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
